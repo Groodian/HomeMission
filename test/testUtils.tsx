bbf290cafb4ccffd8cd97fb6ffe0f24ae58b6913
Clemens Pohle <clemens.pohle@stud.h-da.de>
2021-12-18 14:03:48 +0100

    Enabled all strict type checking options

--- test/testUtils.tsx
+++ test/testUtils.tsx
@@ -3,8 +3,8 @@ import { render, RenderResult } from '@testing-library/react';
 // import { TranslationProvider } from "my-i18n-lib"
 // import defaultStrings from "i18n/en-x-default"
 
-const Providers = ({ children }) => {
-  return children;
+const Providers: React.FC = ({ children }) => {
+  return <>{children}</>;
   // return (
   //   <ThemeProvider theme="light">
   //     <TranslationProvider messages={defaultStrings}>
