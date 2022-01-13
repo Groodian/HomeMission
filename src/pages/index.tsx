188998f374c4527117272ecc1def105d86bfec94
Hai Ninh Bui <ninh.bui@stud.h-da.de>
2022-01-13 10:54:13 +0700

    no message

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,6 +29,11 @@ const Home: NextPage = () => {
         <Link href="/about" color="secondary">
           {t('link-about')}
         </Link>
+
+        <Link href="statistic_page" color="secondary">
+          {t('link-statistic')}
+        </Link>
+
         <Link href="/roommates" color="secondary">
           {t('link-roommates')}
         </Link>
