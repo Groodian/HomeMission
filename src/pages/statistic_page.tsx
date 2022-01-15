45ebe8074eecd93f8a8f90e55af7c174ba968d6f
Hai Ninh Bui <ninh.bui@stud.h-da.de>
2022-01-16 02:21:32 +0700

    leftBar component update

--- src/pages/statistic_page.tsx
+++ src/pages/statistic_page.tsx
@@ -3,24 +3,9 @@ import {useTranslation} from "next-i18next";
 import {serverSideTranslations} from "next-i18next/serverSideTranslations";
 
 const StatisticPage: NextPage = () => {
-  const { t } = useTranslation('statistic');
-
   return (
     <h1>Aloo</h1>
   );
 };
-
-export const getStaticProps: GetStaticProps = async ({ locale }) => {
-  return {
-    props: {
-      ...(await serverSideTranslations(locale || '', [
-        '_app',
-        'roommates',
-        'common',
-      ])),
-    },
-  };
-};
-
 
 export default StatisticPage;
---
+++ src/styles/theme.ts
@@ -0,0 +1,17 @@
+import { createTheme } from '@mui/material/styles';
+import { red, blue, grey } from "@material-ui/core/colors";
+import { makeStyles, createStyles } from '@mui/styles';
+
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: grey[50],
+    },
+  },
+  myButton: {
+    backgroundColor: "red",
+    color: "white",
+    border: "1px solid black",
+  },
+});
