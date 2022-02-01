14ebad9e8a864e4adf0503ac09630753c426ebee
Tom Marek <tom.marek@stud.h-da.de>
2022-02-01 20:05:24 +0100

    Added signin page

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,25 +1,106 @@
-import { withPageAuthRequired } from '@auth0/nextjs-auth0';
 import { GetStaticProps, NextPage } from 'next';
 import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { makeStyles } from '@mui/styles';
+import { Button } from '@mui/material';
+import Image from 'next/image';
+import homeMissionLogo from '../../public/home_mission_grey.png';
+import { useRouter } from 'next/router';
+
+const useStyles = makeStyles(() => ({
+  header: {
+    display: 'flex',
+    background:
+      'radial-gradient(circle at 3% 25%, rgba(0, 40, 83, 1) 0%, rgba(4, 12, 24, 1) 25%)',
+    padding: '4rem 6rem',
+  },
+  headerContent: {
+    flex: '1',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    flexDirection: 'column',
+    marginRight: '5rem',
+  },
+  title: {
+    background: 'linear-gradient(89.97deg, #AE67FA 1.84%, #F49867 102.67%)',
+    backgroundClip: 'text',
+    WebkitBackgroundClip: 'text',
+    WebkitTextFillColor: 'transparent',
+    fontWeight: '800',
+    fontSize: '62px',
+    lineHeight: '75px',
+  },
+  welcomeText: {
+    color: '#81AFDD',
+    fontWeight: 'normal',
+    fontStyle: 'normal',
+    fontSize: '20px',
+    lineHeight: '27px',
+    marginTop: '1.5rem',
+  },
+  getStartedButton: {
+    background: '#FF4820',
+    borderRadius: '5px 5px 5px 5px',
+    color: '#FFFFFF',
+    fontWeight: 'bold',
+    fontStyle: 'normal',
+    fontSize: '20px',
+    lineHeight: '27px',
+    cursor: 'pointer',
+    outline: 'none',
+    border: 'none',
+    paddingLeft: '2rem',
+    paddingRight: '2rem',
+    marginTop: '2rem',
+    textTransform: 'none',
+    '&:hover': {
+      background: '#FC6342',
+    },
+  },
+  image: {
+    flex: '1',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+}));
 
 const Signin: NextPage = () => {
-  const { t } = useTranslation(['signin', 'common']);
+  const classes = useStyles();
+  const router = useRouter();
+  const { t } = useTranslation('signin');
 
   return (
-    <>
-      <h1>{t('welcome-text')}</h1>
-      <h1>{t('welcome-text')}</h1>
-    </>
+    <div className={classes.header}>
+      <div className={classes.headerContent}>
+        <h1 className={classes.title}>{t('title')}</h1>
+        <p className={classes.welcomeText}> {t('welcome-text')}</p>
+        <Button
+          className={classes.getStartedButton}
+          onClick={() => router.push('/api/auth/login')}
+        >
+          {t('button')}
+        </Button>
+      </div>
+      <div className={classes.image}>
+        <Image
+          alt={t('title')}
+          src={homeMissionLogo}
+          width="340px"
+          height="340px"
+        />
+      </div>
+    </div>
   );
 };
 
 export const getStaticProps: GetStaticProps = async ({ locale }) => {
   return {
     props: {
-      ...(await serverSideTranslations(locale || '', ['signin', 'common'])),
+      ...(await serverSideTranslations(locale || '', ['signin'])),
     },
   };
 };
 
-export default withPageAuthRequired(Signin);
+export default Signin;
