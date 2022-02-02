775849a71905224b6f0ffd3b1ab6528d4b6042e6
Clemens Pohle <clemens.pohle@stud.h-da.de>
2022-02-02 10:46:19 +0100

    Made welcome page new index.tsx - The old index.tsx is now test.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,92 +1,97 @@
-import { Button, Link as MuiLink } from '@mui/material';
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
+import { Button } from '@mui/material';
+import { makeStyles } from '@mui/styles';
 import { GetStaticProps, NextPage } from 'next';
 import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-import Copyright from '../components/Copyright';
-import Link from '../components/Link';
-import ProTip from '../components/ProTip';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+import homeMissionLogo from '../../public/home_mission_grey.png';
 
-const Home: NextPage = () => {
-  const { t } = useTranslation('index');
+const useStyles = makeStyles(() => ({
+  header: {
+    display: 'flex',
+    background:
+      'radial-gradient(circle at 3% 25%, rgba(0, 40, 83, 1) 0%, rgba(4, 12, 24, 1) 25%)',
+    padding: '4rem 6rem',
+  },
+  headerContent: {
+    flex: '1',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    flexDirection: 'column',
+    marginRight: '5rem',
+  },
+  title: {
+    background: 'linear-gradient(89.97deg, #AE67FA 1.84%, #F49867 102.67%)',
+    backgroundClip: 'text',
+    WebkitBackgroundClip: 'text',
+    WebkitTextFillColor: 'transparent',
+    fontWeight: '800',
+    fontSize: '62px',
+    lineHeight: '75px',
+  },
+  welcomeText: {
+    color: '#81AFDD',
+    fontWeight: 'normal',
+    fontStyle: 'normal',
+    fontSize: '20px',
+    lineHeight: '27px',
+    marginTop: '1.5rem',
+  },
+  getStartedButton: {
+    background: '#FF4820',
+    borderRadius: '5px 5px 5px 5px',
+    color: '#FFFFFF',
+    fontWeight: 'bold',
+    fontStyle: 'normal',
+    fontSize: '20px',
+    lineHeight: '27px',
+    cursor: 'pointer',
+    outline: 'none',
+    border: 'none',
+    paddingLeft: '2rem',
+    paddingRight: '2rem',
+    marginTop: '2rem',
+    textTransform: 'none',
+    '&:hover': {
+      background: '#FC6342',
+    },
+  },
+  image: {
+    flex: '1',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+}));
+
+const Welcome: NextPage = () => {
+  const classes = useStyles();
+  const router = useRouter();
+  const { t } = useTranslation(['index', 'common']);
 
   return (
-    <Container maxWidth="lg">
-      <Box
-        sx={{
-          my: 4,
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <Typography variant="h4" component="h1" gutterBottom>
-          {t('heading')}
-        </Typography>
-        <Link href="/about" color="secondary">
-          {t('link-about')}
-        </Link>
-
-        <Link href="statistic_page" color="secondary">
-          {t('Go To Statistic Page')}
-        </Link>
-
-        <Link href="/roommates" color="secondary">
-          {t('link-roommates')}
-        </Link>
-        {/* ! Don't use Next link for API */}
-        <MuiLink href="/api/graphql" color="secondary">
-          {t('link-playground')}
-        </MuiLink>
-        <Link href="/auth0" color="secondary">
-          {t('link-auth0')}
-        </Link>
-        <Link href="/join" color="secondary">
-          {t('link-join')}
-        </Link>
-        <Link href="/history" color="secondary">
-          {t('link-history')}
-        </Link>
-        <Link href="/tasks" color="secondary">
-          {t('link-tasks')}
-        </Link>
-        <Link href="/signin" color="secondary">
-          {t('link-signin')}
-        </Link>
-        <Link locale="en" href="" color="secondary">
-          EN
-        </Link>
-        <Link locale="de" href="" color="secondary">
-          DE
-        </Link>
-        <ProTip />
-        <Copyright />
-
-        {/* Content from old index.tsx */}
-        <Button variant="contained" onClick={() => window.alert(t('alert'))}>
-          {t('test-button')}
+    <div className={classes.header}>
+      <div className={classes.headerContent}>
+        <h1 className={classes.title}>{t('title', { ns: 'common' })}</h1>
+        <p className={classes.welcomeText}> {t('welcome-text')}</p>
+        <Button
+          className={classes.getStartedButton}
+          onClick={() => router.push('/api/auth/login')}
+        >
+          {t('button')}
         </Button>
-
-        <a href="https://nextjs.org/docs">
-          <h2>{t('link-documentation')} &rarr;</h2>
-        </a>
-
-        <a href="https://nextjs.org/learn">
-          <h2>{t('link-learn')} &rarr;</h2>
-        </a>
-
-        <a href="https://github.com/vercel/next.js/tree/master/examples">
-          <h2>{t('link-examples')} &rarr;</h2>
-        </a>
-
-        <a href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app">
-          <h2>{t('link-deploy')} &rarr;</h2>
-        </a>
-      </Box>
-    </Container>
+      </div>
+      <div className={classes.image}>
+        <Image
+          alt={t('title', { ns: 'common' })}
+          src={homeMissionLogo}
+          width="340px"
+          height="340px"
+        />
+      </div>
+    </div>
   );
 };
 
@@ -95,8 +100,6 @@ export const getStaticProps: GetStaticProps = async ({ locale }) => {
     props: {
       ...(await serverSideTranslations(locale || '', [
         'index',
-        'Copyright',
-        'ProTip',
         'common',
         'Navbar',
       ])),
@@ -104,4 +107,4 @@ export const getStaticProps: GetStaticProps = async ({ locale }) => {
   };
 };
 
-export default Home;
+export default Welcome;
--- test/pages/index.test.tsx
+++ test/pages/index.test.tsx
@@ -1,31 +1,17 @@
 import React from 'react';
-import Home from '../../src/pages/index';
-import { fireEvent, render } from './testUtils';
+import Welcome from '../../src/pages';
+import { render } from './testUtils';
 
 jest.mock('next/router', () => require('next-router-mock'));
 
-describe('Home page', () => {
+describe('Welcome page', () => {
   it('matches snapshot', async () => {
-    const { asFragment } = await render(<Home />);
+    const { asFragment } = await render(<Welcome />);
     expect(asFragment()).toMatchSnapshot();
   });
 
   it('matches snapshot DE', async () => {
-    const { asFragment } = await render(<Home />, { language: 'de' });
+    const { asFragment } = await render(<Welcome />, { language: 'de' });
     expect(asFragment()).toMatchSnapshot();
-  });
-
-  it('clicking button triggers alert', async () => {
-    const { getByText } = await render(<Home />);
-    window.alert = jest.fn();
-    fireEvent.click(getByText('Test Button'));
-    expect(window.alert).toHaveBeenCalledWith('With typescript and Jest');
-  });
-
-  it('clicking button triggers alert DE', async () => {
-    const { getByText } = await render(<Home />, { language: 'de' });
-    window.alert = jest.fn();
-    fireEvent.click(getByText('Test Button'));
-    expect(window.alert).toHaveBeenCalledWith('Mit typescript und Jest');
   });
 });
