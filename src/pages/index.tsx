4436a2572dd76c0113ba8eb0c0ae7293f4cd004f
Clemens Pohle <clemens.pohle@stud.h-da.de>
2022-02-02 11:25:29 +0100

    Use MUI components for welcome page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
-import { Button } from '@mui/material';
-import { makeStyles } from '@mui/styles';
+import { Button, Container, Grid, Typography } from '@mui/material';
 import { GetStaticProps, NextPage } from 'next';
 import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
@@ -7,91 +6,70 @@ import Image from 'next/image';
 import { useRouter } from 'next/router';
 import homeMissionLogo from '../../public/home_mission_grey.png';
 
-const useStyles = makeStyles(() => ({
-  header: {
-    display: 'flex',
-    background:
-      'radial-gradient(circle at 3% 25%, rgba(0, 40, 83, 1) 0%, rgba(4, 12, 24, 1) 25%)',
-    padding: '4rem 6rem',
-  },
-  headerContent: {
-    flex: '1',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'flex-start',
-    flexDirection: 'column',
-    marginRight: '5rem',
-  },
-  title: {
-    background: 'linear-gradient(89.97deg, #AE67FA 1.84%, #F49867 102.67%)',
-    backgroundClip: 'text',
-    WebkitBackgroundClip: 'text',
-    WebkitTextFillColor: 'transparent',
-    fontWeight: '800',
-    fontSize: '62px',
-    lineHeight: '75px',
-  },
-  welcomeText: {
-    color: '#81AFDD',
-    fontWeight: 'normal',
-    fontStyle: 'normal',
-    fontSize: '20px',
-    lineHeight: '27px',
-    marginTop: '1.5rem',
-  },
-  getStartedButton: {
-    background: '#FF4820',
-    borderRadius: '5px 5px 5px 5px',
-    color: '#FFFFFF',
-    fontWeight: 'bold',
-    fontStyle: 'normal',
-    fontSize: '20px',
-    lineHeight: '27px',
-    cursor: 'pointer',
-    outline: 'none',
-    border: 'none',
-    paddingLeft: '2rem',
-    paddingRight: '2rem',
-    marginTop: '2rem',
-    textTransform: 'none',
-    '&:hover': {
-      background: '#FC6342',
-    },
-  },
-  image: {
-    flex: '1',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-}));
-
 const Welcome: NextPage = () => {
-  const classes = useStyles();
-  const router = useRouter();
   const { t } = useTranslation(['index', 'common']);
+  const router = useRouter();
 
   return (
-    <div className={classes.header}>
-      <div className={classes.headerContent}>
-        <h1 className={classes.title}>{t('title', { ns: 'common' })}</h1>
-        <p className={classes.welcomeText}> {t('welcome-text')}</p>
-        <Button
-          className={classes.getStartedButton}
-          onClick={() => router.push('/api/auth/login')}
-        >
-          {t('button')}
-        </Button>
-      </div>
-      <div className={classes.image}>
-        <Image
-          alt={t('title', { ns: 'common' })}
-          src={homeMissionLogo}
-          width="340px"
-          height="340px"
-        />
-      </div>
-    </div>
+    <Container
+      sx={{
+        background:
+          'radial-gradient(circle at 3% 25%, rgba(0, 40, 83, 1) 0%, rgba(4, 12, 24, 1) 25%)',
+      }}
+    >
+      <Grid container p={8}>
+        <Grid item xs={8}>
+          <Typography
+            variant="h2"
+            component="h1"
+            sx={{
+              background:
+                'linear-gradient(89.97deg, #AE67FA 1.84%, #F49867 102.67%)',
+              backgroundClip: 'text',
+              WebkitTextFillColor: 'transparent',
+              fontWeight: 800,
+            }}
+          >
+            {t('title', { ns: 'common' })}
+          </Typography>
+          <Typography
+            sx={{
+              color: '#81AFDD',
+              fontSize: 20,
+              marginTop: '1.5rem',
+            }}
+          >
+            {t('welcome-text')}
+          </Typography>
+          <Button
+            sx={{
+              background: '#FF4820',
+              color: '#FFFFFF',
+              fontWeight: 'bold',
+              fontSize: 20,
+              paddingLeft: '2rem',
+              paddingRight: '2rem',
+              marginTop: '2rem',
+              textTransform: 'none',
+              '&:hover': {
+                background: '#FC6342',
+              },
+            }}
+            onClick={() => router.push('/api/auth/login')}
+          >
+            {t('button')}
+          </Button>
+        </Grid>
+        <Grid item xs={4}>
+          <Image
+            alt={t('title', { ns: 'common' })}
+            src={homeMissionLogo}
+            width="340px"
+            height="340px"
+          />
+        </Grid>
+      </Grid>
+    </Container>
   );
 };
 
