ecdabe69fa18e04d039133f80759f30fa24e553e
Michael Sudhaus <michael.sudhaus@stud.h-da.de>
2022-02-09 00:11:04 +0100

    Changed color of diamond icon to baby blue

--- src/components/InlineDiamond.tsx
+++ src/components/InlineDiamond.tsx
@@ -1,7 +1,7 @@
 import { Diamond } from '@mui/icons-material';
 
 const InlineDiamond: React.FC = () => (
-  <Diamond fontSize="small" sx={{ verticalAlign: 'sub' }} />
+  <Diamond fontSize="small" sx={{ verticalAlign: 'sub', color: '#29b6f6' }} />
 );
 
 export default InlineDiamond;
